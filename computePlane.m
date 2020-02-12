f71c459d62e36819810c69b5394e940777958481
brucejk <bjhuang@umich.edu>
2020-02-12 17:51:23 -0500

    allow different types of input and make sure it outputs column vectors

--- computePlane.m
+++ computePlane.m
@@ -1,6 +1,26 @@
-function [normal, centroid] = computePlane(vertices_mat)
+function [normal, centroid] = computePlane(object)
+    if isstruct(object)
+        if isfield(object, 'object_vertices')
+            vertices_mat = convertXYZstructToXYZmatrix(object.object_vertices);
+        else
+            error("No 'ojbect_vertices' field in the input, Please check again.")
+        end
+    else
+        vertices_mat = object;
+    end
+
+    if size(vertices_mat, 2) < size(vertices_mat, 1)
+        vertices_mat = vertices_mat';
+    end
+    
     centroid = mean(vertices_mat, 2);
     [U, ~, ~] = svd(vertices_mat - centroid);
     normal = U(:, 3);
-    normal = normal/norm(normal);
+    normal = normal/norm(normal);       
+    if normal(1) < 0
+        normal = -normal;
+    end
+        
+    centroid = makeColumn(centroid);
+    normal = makeColumn(normal);
 end
