bb5fd43168e5eecf9ce17edfebe1a5fd84f99c43
FENGChenxi0823 <cfeng@connect.ust.hk>
2020-02-19 17:01:36 -0500

    script for training and validation process

---
+++ simulateCalibratedLiDAR.m
@@ -0,0 +1,41 @@
+function [object_list, LiDAR_ring_points] = simulateCalibratedLiDAR(object_list, LiDAR_ring_points, LiDAR_opts, delta)
+    num_beam = LiDAR_opts.properties.beam;
+    num_obj = length(object_list);
+    
+    %calibrate LiDAR ring points
+%     for ring_num = 1:num_beam
+%         LiDAR_ring_points(ring_num).homogenous_points = [LiDAR_ring_points(ring_num).points.x;
+%                                                          LiDAR_ring_points(ring_num).points.y;
+%                                                          LiDAR_ring_points(ring_num).points.z;
+%                                                          ones(size(LiDAR_ring_points(ring_num).points.x))];
+%         calibrated_points = delta(ring_num).Affine * LiDAR_ring_points(ring_num).homogenous_points;
+%         LiDAR_ring_points(ring_num).calibrated_points.x = calibrated_points(1,:);
+%         LiDAR_ring_points(ring_num).calibrated_points.y = calibrated_points(2,:);
+%         LiDAR_ring_points(ring_num).calibrated_points.z = calibrated_points(3,:);
+%     end
+    
+    %calibrate objects points
+    for object = 1:num_obj
+        object_list(object).calibrated_points_mat = [];
+        for ring_num = 1:num_beam
+            original_points = [object_list(object).ring_points(ring_num).x;
+                               object_list(object).ring_points(ring_num).y;
+                               object_list(object).ring_points(ring_num).z;
+                               ones(size(object_list(object).ring_points(ring_num).x))];
+            if ~isempty(original_points)
+                % be careful with the indexing here. 0 or 1
+                calibrated_points = delta(ring_num+1).Affine*original_points;
+                object_list(object).calibrated_ring_points(ring_num).x = calibrated_points(1,:);
+                object_list(object).calibrated_ring_points(ring_num).y = calibrated_points(2,:);
+                object_list(object).calibrated_ring_points(ring_num).z = calibrated_points(3,:);
+                calibrated_points_XYZIR = [object_list(object).calibrated_ring_points(ring_num).x; ...
+                                           object_list(object).calibrated_ring_points(ring_num).y; ...
+                                           object_list(object).calibrated_ring_points(ring_num).z; ...
+                                           object_list(object).ring_points(ring_num).I; ...
+                                           object_list(object).ring_points(ring_num).R];
+                object_list(object).calibrated_points_mat = [object_list(object).calibrated_points_mat, calibrated_points_XYZIR ];
+        
+            end
+        end
+    end
+end
