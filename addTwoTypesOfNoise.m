01224d0d4167e6d5b1d0e08ed96b15b520ec1ab8
brucejk <bjhuang@umich.edu>
2020-02-24 14:29:28 -0500

    added error outputs

--- addTwoTypesOfNoise.m
+++ addTwoTypesOfNoise.m
@@ -6,11 +6,15 @@ function noisy_point = addTwoTypesOfNoise(point, sensor_noise, model_noise)
                         sensor_noise.rand_z + model_noise.z;
                         0; 0];
     elseif model_noise.type == "transformation"
-        noisy_point = model_noise.H * point(1:3,:);
-        noisy_point = noisy_point  + ...
+        [~, point_h] = checkHomogeneousCoord(point(1:3));
+        noisy_point = model_noise.sim3 * point_h;
+        point(1:3) = noisy_point(1:3);
+        noisy_point = point  + ...
                        [sensor_noise.rand_x; ...
                         sensor_noise.rand_y; ...
                         sensor_noise.rand_z;
                         0; 0];
+    else
+        error("No such noise type: %s, model_noise.type")
     end
 end
