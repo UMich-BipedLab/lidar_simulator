1540f93dcc39c77502c16394ca2af93c4f0a3a8f
brucejk <bjhuang@umich.edu>
2020-02-24 22:35:21 -0500

    add more noise

--- simpleHomogeneousNoiseModelAddOnNoise.m
+++ simpleHomogeneousNoiseModelAddOnNoise.m
@@ -3,6 +3,6 @@ function noise_model = simpleHomogeneousNoiseModelAddOnNoise(ring_num, LiDAR_opt
     RT_mechanismThreeParams = mechanismThreeParamsToSim3(0, 0, noise_model.range_noise, noise_model.az_noise, noise_model.el_noise);
     noise_model.type = 'transformation';
     noise_model.sim3  = RT_mechanismThreeParams;
-    noise_addon = rotx(2);% 10 degs
+    noise_addon = rotx(1)*roty(2)*rotz(-1.5);% 10 degs
     noise_model.sim3(1:3,1:3) = RT_mechanismThreeParams(1:3, 1:3) * noise_addon;
 end
