0de345987be63da7c796883ebd258f378da3cd58
brucejk <bjhuang@umich.edu>
2020-03-06 13:57:17 -0500

    add random seed

--- genRandomNumber.m
+++ genRandomNumber.m
@@ -1,4 +1,5 @@
-function rand_num = genRandomNumber(a, b, seed, N)
+function rand_num = genRandomNumber(range_min, range_max, seed, N)
+    % return (N,1) random numbers in between range_minand range_max 
     if exist('seed', 'var')
         rng(seed)
     else
@@ -7,5 +8,11 @@ function rand_num = genRandomNumber(a, b, seed, N)
     if ~exist("N", "var")
         N = 1;
     end
-    rand_num = a + (b-a) .* rand(N, 1);
+    rand_num = range_min + (range_max-range_min) .* rand(N, 1);
 end
+% 
+% 
+%     range_max =  0.005;
+%     range_max =  0.1;
+%     range_min = -range_max;
+%     noise_model.range_noise = genRandomNumber(range_min, range_max, ring_num); 
