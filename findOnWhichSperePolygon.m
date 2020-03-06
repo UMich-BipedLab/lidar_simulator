866e97f350b740dbf445082e1c3b5312e7ccc29c
brucejk <bjhuang@umich.edu>
2020-03-06 14:38:19 -0500

    return polygon and unit point on xy plane

---
+++ findOnWhichSperePolygon.m
@@ -0,0 +1,44 @@
+function [which_polygon, plane, uinit_point_xy] = findOnWhichSperePolygon(point, size, division, r)
+    if ~exist('r', 'var')
+        r = 2;
+    end
+    if ~exist('division', 'var')
+        division = 4;
+    end
+    point_unit = point ./ size;
+    [x, y, z] = sphere(division); 
+    x = x(division:end,:);
+    y = y(division:end,:);
+    z = z(division:end,:); 
+    z = z - z(1,1); % move to (x, y, 0)
+
+    a = gradient(z);
+    fig_handle = createFigHandle(1, "sphere");
+    hs = surf(fig_handle, r.*x,r.*y,r.*z, 'AlphaData',a, 'FaceAlpha','flat',...
+        'FaceColor','blue');
+    direction = [0 0 1]; 
+    rotate(hs, direction, 180/division) 
+    XData = hs.XData;
+    YData = hs.YData;
+    ZData = hs.ZData;
+    uinit_point_xy = [point_unit(3), point_unit(2), point_unit(1)];
+    for i = 1:division
+    %     j = mod(j, 4);
+        plane(i).object_vertices.x = [0 XData(1, i), XData(1, i+1)];
+        plane(i).object_vertices.y = [0 YData(1, i), YData(1, i+1)];
+        plane(i).object_vertices.z = [ZData(2,i) ZData(1, i), ZData(1, i+1)];
+
+        plane(i).triangle.x = [0 XData(1, i), XData(1, i+1)];
+        plane(i).triangle.y = [0 YData(1, i), YData(1, i+1)];
+        plane(i).triangle.z = [0 ZData(1, i), ZData(1, i+1)];
+        [plane(i).normal, plane(i).centroid, plane(i).R] = computePlaneReturnR(plane(i));
+    %     scatter3(plane(i).triangle.x, plane(i).triangle.y, plane(i).triangle.z, 'y', 'fill')
+        plane(i).angle_sum = computeAngleSum(plane(i).triangle, uinit_point_xy); 
+        plane(i).status = 0;
+        if abs(plane(i).angle_sum - 2*pi) < 1e-5
+            plane(i).status = 1;
+            interior = point;
+        end
+    end
+    [~, which_polygon] = min(abs([plane(:).angle_sum]- 2*pi));
+end
